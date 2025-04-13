f310eeef35bc2ae79b64d61a3aa35fcb3356483d
LockeSarah <sarahlocke2001@gmail.com>
2025-04-12 23:17:38 -0400

    2

--- api/index.js
+++ api/index.js
@@ -22,5 +22,41 @@ app.get("/",(req,res)=>{
   res.send(" Sorry Error")
   }
 });
+// Display all movies
+app.get("/movies", async (req, res) => {
+  try {
+      const result = await pool.query("SELECT * FROM movies");
+      res.json({ rows: result.rows });
+      // console.log(result.rows.length);
+  } catch (error) {
+      console.error("Query error:", error);
+      res.status(500).json({ error: "Database query failed" })
+  }
+});
+// Display specific movie
+app.get("/getMovie", async (req, res) => {
+  try {
+      var id = req.query.id;
+      const result = await pool.query("SELECT * FROM movies WHERE id =" + id);
+      res.json({ rows: result.rows });
+  } catch (error) {
+      console.error("Query error:", error);
+      res.status(500).json({ error: "Database query failed" })
+  }
+});
+
+// Delete specific movie
+app.get("/deleteMovie", async (req, res) => {
+  try {
+      var id = req.query.id;
+      console.log(id);
+      const result = await pool.query("DELETE * FROM movie WHERE id =" + id);
+      console.log(result);
+      res.json({ ans: 1 });
+  } catch (error) {
+      console.error("Query error:", error);
+      res.json({ ans: 0});
+  }
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
